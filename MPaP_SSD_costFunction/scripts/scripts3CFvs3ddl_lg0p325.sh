9051fbde01b7642a566c4d5ecef26e6dcf503696
Guillaume Caron <guillaume.caron@u-picardie.fr>
2020-04-22 04:41:37 +0200

    add Mixture of Photometric anisotropic Potentials SSD cost function project

---
+++ MPaP_SSD_costFunction/scripts/scripts3CFvs3ddl_lg0p325.sh
@@ -0,0 +1,1 @@
+./MPPSSDcostFunction /Users/guillaume/Acquisitions/gyrovisu/spherique/gyro/SVMIS/calib/resultats/calib_subdiv3.xml 3 0.05 /Users/guillaume/Acquisitions/gyrovisu/spherique/gyro/SVMISp/ThreeDOFs/subdiv3/ 0 0 72 /Users/guillaume/Acquisitions/gyrovisu/spherique/gyro/SVMIS/OneDOF/subdiv3/maskFull.png;
